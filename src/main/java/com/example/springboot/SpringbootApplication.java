a03ef00701eebccc1cb867c89b7e319701920796
Pedro Souza <contato.pedromoc@gmail.com>
2025-01-28 15:13:24 -0300

    first commit

---
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,11 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel implements Serializable {
+}
