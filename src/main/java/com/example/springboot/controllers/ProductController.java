05d42c5768993406be40449af3228d9dc86e6b79
Pedro Souza <contato.pedromoc@gmail.com>
2025-01-28 21:50:43 -0300

    added HATEOAS in /products

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -47,6 +47,7 @@ public class ProductController {
         if (productO.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
+        productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products List"));
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
 
